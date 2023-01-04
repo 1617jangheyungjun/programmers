ba7362a67928ab5e978bbe1a7b06e641df86c315
jangheyoungjun <jangh4139@gmail.com>
2023-01-04 13:26:24 +0900

    품

---
+++ 가장 큰 수 찾기/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class Solution
+{
+    public int[] solution(int[] array)
+    {
+        int[] answer = new int[2];
+        {
+            int k = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    k = i;
+                }
+                else
+                {
+                    k = i + 1;
+                }
+            }
+            answer[0] = array[k];
+            answer[1] = k;
+        }
+        return answer;
+    }
+        
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] check = new int[3] { 1, 8, 3 }
+
+
+        Console.WriteLine(Solution.solution(check))
+    }
+}
---
+++ 가장 큰 수 찾기/다음에 올 수 찾기/다음에 올 수 찾기/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+public class Solution
+{
+    public int solution(int[] common)
+    {
+        int answer = 0;
+        if(common[0] - common[1] == common[1] - common[2])
+        {
+            int m = common[2] - common[1];
+            answer = common[^1] + m;
+        }
+        else
+        {
+            int m = common[2] / common[1];
+            answer = common[common.Length - 1] * m;
+        }
+        return answer;  
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] common = new int[] {2,4,8,16};
+        Console.WriteLine(solution.solution(common));
+    }
+}
+
+
